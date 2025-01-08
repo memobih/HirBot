1b5954f7f93116db2be5f27499f7a0f1bcc1d211
mohamed abdo <abdomohamrd59@gmail.com>
2025-01-08 23:26:29 +0200

    ok

--- HirBot.Infrastructure/Project.Repository/obj/Release/net8.0/HireBot.Repository.AssemblyInfo.cs
+++ HirBot.Infrastructure/Project.Repository/obj/Release/net8.0/HireBot.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HireBot.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c18b8aa7316b3d3c804be5159b20ef0df9968e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6de85cdc8d7cbfbbb196a78d2cfbee198cc5d10")]
 [assembly: System.Reflection.AssemblyProductAttribute("HireBot.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HireBot.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
