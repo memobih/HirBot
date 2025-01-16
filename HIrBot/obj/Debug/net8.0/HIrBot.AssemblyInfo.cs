cfbe3e5d17c417159c186a6629ddf0f0b71df5a5
mohamed abdo <abdomohamrd59@gmail.com>
2025-01-16 21:34:43 +0200

    ok

--- HIrBot/obj/Debug/net8.0/HIrBot.AssemblyInfo.cs
+++ HIrBot/obj/Debug/net8.0/HIrBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HirBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f77d3ddc9f419e80e9d1455efcca645e28d056")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6ce209218d193fdee996c10caa5393b44ee5ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("HirBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HirBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
