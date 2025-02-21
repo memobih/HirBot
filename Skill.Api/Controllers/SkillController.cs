f02282f891543bca7a76dee201b574b7a8bb99bd
mohamed abdo <abdomohamrd59@gmail.com>
2025-02-22 00:18:31 +0200

    last okkkkkk

---
+++ Skill.Api/Controllers/SkillController.cs
@@ -0,0 +1,41 @@
+﻿
+using HirBot.Comman.Idenitity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project.ResponseHandler.Models;
+using skill.services.DataTransferObjects;
+using skill.services.Interfaces;
+
+namespace skill.api.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class SkillController : ApiControllerBase
+    {
+        private readonly ILogger<SkillController> _logger;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ISkillService _skillService;
+
+        public SkillController(ILogger<SkillController> logger, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, ISkillService skillService)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _skillService = skillService;
+        }
+
+
+        [HttpPost("AddSkill")]
+        public async Task<IActionResult> AddSkill(AddSkillDto skill)
+        {
+            var result = await _skillService.AddSkill(skill);
+            if (result.Succeeded) return Ok(result);
+            return BadRequest(result);
+        }
+    }
+}
