b2fbb6193605760a74ccff04d79057a6a7c25e66
mahmoudnourr <graphh37@gmail.com>
2025-04-17 16:58:33 +0200

    Add migration for updating Interview and Company tables
    - Altered the 'InterviewerName' column in the 'Interviews' table to allow null values.
    - Added a new 'Name' column to the 'Companies' table with a maximum length of 200 characters and a default value of an empty string.

---
+++ HirBot.Infrastructure/Project.EntityFramework/Migrations/20250417145620_dd.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HirBot.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class dd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InterviewerName",
+                table: "Interviews",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Companies",
+                type: "varchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Companies");
+
+            migrationBuilder.UpdateData(
+                table: "Interviews",
+                keyColumn: "InterviewerName",
+                keyValue: null,
+                column: "InterviewerName",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InterviewerName",
+                table: "Interviews",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
