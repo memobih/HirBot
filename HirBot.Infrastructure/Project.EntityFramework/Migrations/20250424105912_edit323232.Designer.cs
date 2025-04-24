862c259feabf800b54e35db38d74196e36d32f2b
mahmoudnourr <graphh37@gmail.com>
2025-04-24 13:00:06 +0200

    fix: Rename Notification columns to follow naming conventions

--- HirBot.Infrastructure/Project.EntityFramework/Migrations/20250424105912_edit323232.Designer.cs
+++ HirBot.Infrastructure/Project.EntityFramework/Migrations/20250424105912_edit323232.Designer.cs
@@ -4,6 +4,7 @@ using HirBot.EntityFramework.DataBaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HirBot.EntityFramework.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250424105912_edit323232")]
+    partial class edit323232
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
